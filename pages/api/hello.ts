5d4b2c30b4ed253e553fbe30b2956f92c6ffaa29
DESKTOP-RM01CT1\aaron <quintanalaaron@gmail.com>
2023-12-03 22:16:57 +0100

    feat: started project

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,12 +2,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  name: string;
+  surname: string;
+  email: string;
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({
+    name: 'Aarón',
+    surname: 'Quintanal Martín',
+    email: 'quintanalaaron@gmail.com'
+  })
 }
