e75394f43efa9bf6ef99d0cfd839e9720614a078
DESKTOP-RM01CT1\aaron <quintanalaaron@gmail.com>
2023-12-05 14:57:23 +0100

    refactor: types

--- database/projects.ts
+++ database/projects.ts
@@ -1,96 +1,36 @@
-import { IProjecListResponse, IProject } from "@/interfaces/project-list";
+import { IProject } from "@/interfaces/project-list";
 
-export const initialData: IProjecListResponse = {
-    projects: [
-        {
-            _id: '1',
-            title: 'AutoTerra',
-            description: 'Terrario automatizado',
-            slug: 'q_tech_autoterra',
-            tag: ['multiplataforma'],
-            type: "personal",
-            image: '/svg/github-mark-white.svg'
-        },
-        {
-            _id: '2',
-            title: 'FuckOffendiditos',
-            description: 'Página de humor',
-            slug: 'fuck_offendiditos',
-            tag: ['webpage'],
-            type: "professional",
-            image: '/svg/github-mark-white.svg'
-        },
-        {
-            _id: '3',
-            title: 'CodeBot',
-            description: 'Generador automático de código',
-            slug: 'code_bot',
-            tag: ['development', 'automation'],
-            type: "personal",
-            image: '/svg/github-mark-white.svg'
-        },
-        {
-            _id: '4',
-            title: 'EcoWatch',
-            description: 'Aplicación para monitoreo ambiental',
-            slug: 'eco_watch',
-            tag: ['mobile', 'environment'],
-            type: "professional",
-            image: '/svg/github-mark-white.svg'
-        },
-        {
-            _id: '5',
-            title: 'HealthTrack',
-            description: 'Plataforma de seguimiento de salud',
-            slug: 'health_track',
-            tag: ['webpage', 'healthcare'],
-            type: "personal",
-            image: '/svg/github-mark-white.svg'
-        },
-        {
-            _id: '6',
-            title: 'SmartHomeHub',
-            description: 'Centro de control para hogar inteligente',
-            slug: 'smart_home_hub',
-            tag: ['iot', 'home-automation'],
-            type: "professional",
-            image: '/svg/github-mark-white.svg'
-        },
-        {
-            _id: '7',
-            title: 'LearnEasy',
-            description: 'Plataforma educativa interactiva',
-            slug: 'learn_easy',
-            tag: ['education', 'e-learning'],
-            type: "personal",
-            image: '/svg/github-mark-white.svg'
-        },
-        {
-            _id: '8',
-            title: 'MarketInsights',
-            description: 'Análisis de mercado en tiempo real',
-            slug: 'market_insights',
-            tag: ['analytics', 'business'],
-            type: "professional",
-            image: '/svg/github-mark-white.svg'
-        },
-        {
-            _id: '9',
-            title: 'FitBuddy',
-            description: 'App de entrenamiento personalizado',
-            slug: 'fit_buddy',
-            tag: ['mobile', 'fitness'],
-            type: "personal",
-            image: '/svg/github-mark-white.svg'
-        },
-        {
-            _id: '10',
-            title: 'GreenGarden',
-            description: 'Gestión de jardines sostenibles',
-            slug: 'green_garden',
-            tag: ['webpage', 'sustainability'],
-            type: "professional",
-            image: '/svg/github-mark-white.svg'
-        }
-    ]
-}
+export const initialData: IProject[] = [
+    {
+        _id: '1',
+        title: [
+            { lang: 'es', value: 'Autoterra' }
+        ],
+        description: [
+            { lang: 'es', value: 'Terrario automatizado' },
+        ],
+        documentation: [
+            {lang: 'es', value: ''}
+        ],
+        github: '',
+        link: '',
+        logo: '',
+        slug: 'q_tech_autoterra',
+        tag: [
+            {lang: 'es', value: 'Multiplataforma'}
+        ],
+        available_languages: ['es'],
+        category: "personal",
+        dateFrom: Date,
+
+    },
+    // {
+    //     _id: '2',
+    //     title: 'FuckOffendiditos',
+    //     description: 'Página de humor',
+    //     slug: 'fuck_offendiditos',
+    //     tag: ['webpage'],
+    //     type: "professional",
+    //     image: '/svg/github-mark-white.svg'
+    // }
+];
--- interfaces/project-list.ts
+++ interfaces/project-list.ts
@@ -1,15 +1,49 @@
-export interface IProjecListResponse {
-    projects: IProject[];
-}
-
 export interface IProject {
     _id: string;
-    title: string;
-    description: string;
-    image?: string;
+    description: IInternationalization<string>[];
+    documentation: IInternationalization<string>[];
+    github?: string;
+    link?: string;
+    logo: string;
     slug: string;
-    tag: string[];
-    type: IType
+    available_languages: ILanguage[];
+    tag: IInternationalization<string>[];
+    title: IInternationalization<string>[];
+    category: IProjectCategory;
+    dateFrom: Date;
+    dateTo?: Date;
+    developers: IDeveloper[];
+    status: IProjectStatus;
 }
 
-export type IType = 'personal' | 'professional' | 'job'
+export interface IDeveloper {
+    _id: string;
+    name: string;
+    surname: string;
+    github?: string;
+    portfoil?: string;
+}
+
+export interface IInternationalization<T> {
+    lang: ILanguage;
+    value: T;
+}
+
+export type ILanguage =
+    | 'es'
+    | 'en'
+
+export type IProjectCategory =
+    | 'production'
+    | 'personal'
+    | 'freelance';
+
+export type IProjectStatus =
+    | 'investigation'
+    | 'planification'
+    | 'designing'
+    | 'developing'
+    | 'launching'
+    | 'maintenance'
+    | 'tracking'
+    | 'finished'
